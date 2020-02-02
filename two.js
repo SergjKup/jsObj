201ec9a1fedd58bf5a809a708fe0b67c9552c7ba
SergjKup <58332288+SergjKup@users.noreply.github.com>
2020-02-02 23:33:32 +0300

    add two task

---
+++ two.js
@@ -0,0 +1,14 @@
+function sequence(start = 0, step = 1) {
+  start -= step; // -1
+  return function() {
+    return (start += step);
+  };
+}
+var generator = sequence(10, 3);
+alert(generator()); // 10
+alert(generator()); // 13
+alert(generator()); // 16
+var generatorMain = sequence();
+alert(generatorMain()); // 0
+alert(generatorMain()); // 1
+alert(generatorMain()); // 2
